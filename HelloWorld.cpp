31f6406f1e94f9891b1fd9f3cd74ac006800ca70
sidneywhitman <141499836+sidneywhitman@users.noreply.github.com>
2023-08-09 20:40:23 -0400

    Update HelloWorld.cpp
    Ask user to enter a number. Print the int value that the user provides.

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -5,6 +5,10 @@ int main()
 	std::cout << "Hello world!\n";
 	std::cout << "It's nice to meet you.\n";
 	std::cout << "What's good with you?\n";
+	int x{};
+	std::cout <<"Enter a number: ";
+	std::cin >> x;
+	std::cout >> "You entered "<< x <<'\n';
 
 	return 0;
 }
